bc560970b4f9ee773a26a6e865fafaa41e706b1b
Murali07 <muraliselvaraj97@gmail.com>
2022-07-22 17:00:25 +0530

    conected to heroku

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 // export const API = "https://6251286d977373573f44d46e.mockapi.io";
 
-// export const API = "https://b32wenode.herokuapp.com";
+export const API = "https://b32wenode.herokuapp.com";
 
-export const API = "http://localhost:4000";
+// export const API = "http://localhost:4000";
